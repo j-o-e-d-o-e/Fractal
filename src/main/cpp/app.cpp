8ed861aaad9f1e8f04db3df03c5c0bb15efe3349
j-o-e-d-o-e <d_joe@gmx.net>
2020-03-10 02:19:47 +0100

    v0.4: refactored/restructured

--- src/main/cpp/app.cpp
+++ src/main/cpp/app.cpp
@@ -1,56 +1,20 @@
 #include <iostream>
-#include <stdlib.h>
-#include <memory>
-#include <math.h>
-#include <cstdint>
-#include <Mandelbrot.h>
-#include "Bitmap.h"
-#include "ZoomList.h"
+#include "FractalCreator.h"
+#include "Zoom.h"
 
 using namespace std;
 using namespace BitmapSpace;
+using namespace FractalSpace;
 
 int main() {
-    int const WIDTH = 800;
-    int const HEIGHT = 600;
-    Bitmap bitmap(WIDTH, HEIGHT);
-    ZoomList zoomList(WIDTH, HEIGHT);
-    zoomList.add(Zoom(WIDTH / 2, HEIGHT / 2, 4.0/WIDTH));
-    zoomList.add(Zoom(295, HEIGHT - 202, 0.1));
-    zoomList.add(Zoom(312, HEIGHT - 304, 0.1));
-    unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS]{});
-    unique_ptr<int[]> fractal(new int[WIDTH * HEIGHT]{});
-    for (int y = 0; y < HEIGHT; y++) {
-        for (int x = 0; x < WIDTH; x++) {
-            pair<double, double> coords = zoomList.doZoom(x, y);
-            int iterations = Mandelbrot::getIterations(coords.first, coords.second);
-            fractal[y * WIDTH + x] = iterations;
-            if (iterations != Mandelbrot::MAX_ITERATIONS) {
-                histogram[iterations]++;
-            }
-        }
-    }
-    int total = 0;
-    for (int i = 0; i < Mandelbrot::MAX_ITERATIONS; i++) {
-        total += histogram[i];
-    }
-    for (int y = 0; y < HEIGHT; y++) {
-        for (int x = 0; x < WIDTH; x++) {
-            uint8_t red = 0;
-            uint8_t green = 0;
-            uint8_t blue = 0;
-            int iterations = fractal[y * WIDTH + x];
-            if (iterations != Mandelbrot::MAX_ITERATIONS) {
-                double hue = 0.0;
-                for (int i = 0; i <= iterations; i++) {
-                    hue += ((double) histogram[i]) / total;
-                }
-                green = pow(255, hue);
-            }
-            bitmap.setPixel(x, y, red, green, blue);
-        }
-    }
-    bitmap.write("test.bmp");
+    int height = 600;
+    FractalCreator fractalCreator(800, 600);
+    fractalCreator.addZoom(Zoom(295, height - 202, 0.1));
+    fractalCreator.addZoom(Zoom(312, height - 304, 0.1));
+    fractalCreator.calculateIteration();
+    fractalCreator.calculateTotalIterations();
+    fractalCreator.drawFractal();
+    fractalCreator.writeBitmap("test.bmp");
     cout << "Finished." << endl;
     return 0;
 }
--- src/main/headers/FractalCreator.h
+++ src/main/headers/FractalCreator.h
@@ -2,19 +2,32 @@
 #define FRACTAL_FRACTALCREATOR_H
 
 #include <string>
+#include <bits/unique_ptr.h>
 #include "Zoom.h"
+#include "Bitmap.h"
+#include "ZoomList.h"
 
 using namespace std;
 using namespace BitmapSpace;
 
 namespace FractalSpace {
     class FractalCreator {
+    private:
+        int m_width;
+        int m_height;
+        unique_ptr<int[]> m_histogram;
+        unique_ptr<int[]> m_fractal;
+        Bitmap m_bitmap;
+        ZoomList m_zoomList;
+        int m_total{0};
     public:
         FractalCreator(int width, int height);
 
         virtual ~FractalCreator();
 
         void calculateIteration();
+
+        void calculateTotalIterations();
 
         void drawFractal();
 
