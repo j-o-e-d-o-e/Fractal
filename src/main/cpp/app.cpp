9ce72f9a3356744b46ee0784842b7bc8dccce295
j-o-e-d-o-e <d_joe@gmx.net>
2020-03-10 14:44:21 +0100

    minor changes

--- src/main/cpp/app.cpp
+++ src/main/cpp/app.cpp
@@ -8,13 +8,13 @@ using namespace FractalSpace;
 int main() {
     FractalCreator fractalCreator(800, 600);
 
-    fractalCreator.addRange(0.0, Rgb(0, 0, 255));
-    fractalCreator.addRange(0.3, Rgb(255, 99, 71)); // iteration 0 to 300
+    fractalCreator.addRange(0.0, Rgb(81, 60, 255));
+    fractalCreator.addRange(0.3, Rgb(255, 52, 67)); // iteration 0 to 300
     fractalCreator.addRange(0.5, Rgb(255, 215, 0)); // 300 to 500
     fractalCreator.addRange(1.0, Rgb(255, 255, 255)); // 500 to 1000
 
-    fractalCreator.addZoom(Zoom(295, 202, 0.1));
-    fractalCreator.addZoom(Zoom(312, 304, 0.1));
+    fractalCreator.addZoom(Zoom(295, 202, 0.05));
+    fractalCreator.addZoom(Zoom(312, 304, 0.05));
 
     fractalCreator.run("Fractal.bmp");
     cout << "Finished." << endl;
